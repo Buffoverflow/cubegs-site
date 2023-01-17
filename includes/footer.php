c265b70350b3cec202d5dd12c72bbe3f32566bdf
Francisco <juniordez@gmail.com>
2023-01-17 14:03:38 -0300

    Rebel and DOTD albums

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+
+
+    <!-- Footer -->
+    <footer id="anita-footer">
+        <div class="anita-footer-inner">
+            <div class="anita-copyright anita-js-copyright"></div>
+            <div class="anita-socials anita-js-socials"></div>
+        </div>
+    </footer>
+
+    <!-- JS Scripts -->
+    <script src="js/lib/jquery.min.js"></script>
+    <script src="js/lib/aos.min.js"></script>
+    <script src="js/core.js"></script>
+</body>
+</html>
